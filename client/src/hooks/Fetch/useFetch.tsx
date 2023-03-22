e8d7492b7b1bf7dda79a098c2fb39a2207728c1c
AlanChou1995 <99056343+AlanChou1995@users.noreply.github.com>
2023-03-22 10:57:02 +0800

    added useFetch custom hook

--- client/src/hooks/Fetch/useFetch.tsx
+++ client/src/hooks/Fetch/useFetch.tsx
@@ -2,9 +2,32 @@ import { useEffect, useState } from 'react'
 import axios from "axios"
 
 // types
+import { FetchDataType } from '../../types/DataType'
+import { FetchDataStateType } from '../../types/DataType'
 
-
-export const useFetch = () => {
-
+const useFetch = ({ initUrl, initData, query }: FetchDataType) => {
+  const [fetchState, setFetchState] = useState<FetchDataStateType>({
+    loading: false,
+    error: false,
+    data: initData,
+    query: ''
+  })
+  useEffect(() => {
+    const getData = async () => {
+      setFetchState({ ...fetchState, loading: true })
+      try {
+        const response = (await axios(initUrl + query)).data
+        setFetchState({ ...fetchState, data: response })
+      }
+      catch (err) {
+        setFetchState({ ...fetchState, error: true })
+        throw new Error('useFetch hook get error data!')
+      }
+      setFetchState({ ...fetchState, loading: false })
+    }
+    getData()
+  }, [query])
+  return ({ fetchState })
 }
 
+export default useFetch
