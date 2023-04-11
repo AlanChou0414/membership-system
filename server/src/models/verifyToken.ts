54fe5bbcf0f121704442425532bae2d2e4dbb3ed
AlanChou1995 <99056343+AlanChou1995@users.noreply.github.com>
2023-04-11 16:51:20 +0800

    added signin feature

---
+++ server/src/models/verifyToken.ts
@@ -0,0 +1,17 @@
+// import { Request, Response, NextFunction } from "express";
+
+// //tools
+// import dotenv from 'dotenv'
+// dotenv.config()
+
+// export const verifyToken = (
+//   req: Request, res: Response, next: NextFunction
+// ) => {
+//   const authHeader = req.header.authorization
+//   if (!authHeader) {
+//     return res.status(400).json({ message: 'Unauthorized' })
+//   }
+//   const token = authHeader.split(' ')[1]
+
+
+// }
