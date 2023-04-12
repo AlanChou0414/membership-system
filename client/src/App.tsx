bef67b1c5f5eb5447c574fff81c26577d2152f6a
AlanChou1995 <99056343+AlanChou1995@users.noreply.github.com>
2023-04-12 17:20:59 +0800

    added publicroutes and home page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,33 +1,48 @@
 import { BrowserRouter, Route, Routes, } from "react-router-dom"
+import { Provider } from "react-redux"
+
+//store
+import store from "./store/configureStore"
 
 // layout
 import MainLayout from "./layouts/Main/MainLayout"
 
 // private
 import PrivateRoutes from "./routes/PrivateRoutes"
+import PublicRoutes from "./routes/PublicRoutes"
 
 // pages
 import Home from "./pages/Home"
 import SignIn from "./pages/User/SignIn"
 import SignUp from "./pages/User/SignUp"
 import Center from "./pages/User/Center"
+import NotFound from "./pages/NotFound"
 
 const App = () => {
-
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<MainLayout />}>
-          <Route element={<PrivateRoutes />}>
-            <Route index element={<Home />} />
+      <Provider store={store}>
+        <Routes>
+          <Route path='/' element={<MainLayout />}>
+            
+            {/* private */}
+            <Route element={<PrivateRoutes />}>
+              <Route index element={<Home />} />
+              <Route path='/center' element={<Center />} />
+            </Route>
+
+            {/* public */}
+            <Route element={<PublicRoutes />}>
+              <Route path='/Signin' element={<SignIn />} />
+              <Route path='/Signup' element={<SignUp />} />
+            </Route>
+
+            {/* notfound */}
+            <Route path="*" element={<NotFound />} />
+
           </Route>
-          <Route path='/Signin' element={<SignIn />} />
-          <Route path='/Signup' element={<SignUp />} />
-          <Route element={<PrivateRoutes />}>
-            <Route path='/center' element={<Center />} />
-          </Route>
-        </Route>
-      </Routes>
+        </Routes>
+      </Provider>
     </BrowserRouter>
   )
 }
--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,11 +1,45 @@
-import { useSelector } from "react-redux"
+import { useSelector, useDispatch } from "react-redux"
+import { useNavigate } from "react-router-dom"
+import jwtDecode from "jwt-decode"
 import { useEffect } from "react"
+import axios from "axios"
+import Cookies from "js-cookie"
 
 //type
 import { RootState } from "../../types/RootState"
+interface DecodedToken {
+  _id: string
+}
+
+//slice
+import { setUserData } from "../../slice/User/UserSlice"
+import { setAlert } from "../../slice/Alert/AlertSlice"
 
 const Home = () => {
   const { userName, userEmail } = useSelector((state: RootState) => state.User)
+  const alertMessage = useSelector((state: RootState) => state.Alert)
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    const token = Cookies.get('token')
+    const decoded: DecodedToken = jwtDecode(token!) as DecodedToken
+    const getUserData = async () => {
+      const response = await axios.get(`${import.meta.env.VITE_VAR_API_URL}/user/${decoded!._id}`)
+      const { data } = response
+      dispatch(setUserData(data.data))
+    }
+    getUserData()
+  }, [])
+
+  const handleUserLogout = () => {
+    Cookies.remove('token')
+    dispatch(setAlert('Logout success!'))
+    setTimeout(() => {
+      navigate('/signin')
+      dispatch(setAlert(''))
+    }, 1000);
+  }
 
   return (
     <section className='flex justify-center items-center m-40'>
@@ -16,8 +50,12 @@ const Home = () => {
           <div>Email : {userEmail}</div>
           <input
             className='border cursor-pointer mt-5 p-2 hover:bg-black hover:text-white transition duration-500 rounded-lg'
-            type="button" value="Logout" />
+            type="button" value="Logout" onClick={handleUserLogout} />
         </div>
+        {
+          alertMessage &&
+          <div className='text-xl mt-3 text-red-400 text-center'>{alertMessage}</div>
+        }
       </div>
     </section>
   )
